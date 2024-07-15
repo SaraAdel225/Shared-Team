8b47f21190d35a5b40f2358a9167d8b70290c8cc
Raheeemsala7 <rahemsalah534@gmail.com>
2024-07-15 23:13:07 +0300

    Login

---
+++ frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,143 @@
+'use client'
+
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    InputGroup,
+    InputRightElement,
+} from '@chakra-ui/react'
+import {
+    FormHelperText,
+} from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { GrView } from "react-icons/gr";
+import { BiHide } from "react-icons/bi";
+// 
+// import { Navigate } from 'react-router-dom';
+
+
+
+export default function SimpleCard() {
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [isEmail, setIsEmail] = useState(false)
+    const [isPasword, setIsPasword] = useState(false)
+    const [user, setUser] = useState({
+        identifier: "",
+        password: "",
+    })
+
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target
+        setUser({ ...user, [name]: value })
+    }
+
+    const onSubmit = (e : React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+
+        if (!user.identifier && !user.password) {
+            setIsEmail(true)
+            setIsPasword(true)
+
+            
+        }
+        if (!user.identifier) {
+            setIsEmail(true)
+
+            return
+        }
+        if (!user.password) {
+            setIsPasword(true)
+            return
+        }
+
+        setIsEmail(false)
+        setIsPasword(false)
+        dispatch(userLogin(user))
+        // window.location.reload()
+        return
+    }
+
+    // if (isAuthenticated) return <Navigate to="/" replace />
+
+    return (
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}>
+            <Stack as={"form"} spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}>Login</Heading>
+                    {/* <Text fontSize={'lg'} color={'gray.600'}>
+                        to enjoy all of our cool <Text color={'blue.400'}>features</Text> ✌️
+                    </Text> */}
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <Stack spacing={4}>
+                        <FormControl id="email">
+                            <FormLabel>Email address</FormLabel>
+                            <Input type="email" name="identifier" value={user.identifier} onChange={(e) => onChangeHandler(e)} isInvalid={isEmail}
+                                errorBorderColor='red.300' required />
+                            {isEmail ? <FormHelperText color={"red.500"}>
+                                Email is required
+                            </FormHelperText> : null}
+                        </FormControl>
+                        <FormControl id="password" isRequired>
+                            <FormLabel>Password</FormLabel>
+                            <InputGroup>
+                                <Input type={showPassword ? 'text' : 'password'} name="password" onChange={(e) => onChangeHandler(e)} isInvalid={isPasword}
+
+                                    errorBorderColor='red.300' value={user.password} required />
+
+                                <InputRightElement h={'full'}>
+                                    <Button
+                                        variant={'ghost'}
+                                        p="0"
+                                        onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                                        {showPassword ? <GrView /> : <BiHide />}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                            {isPasword ? <FormHelperText color={"red.500"}>
+                                Password is required
+                            </FormHelperText> : null}
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Stack
+                                direction={{ base: 'column', sm: 'row' }}
+                                align={'start'}
+                                justify={'flex-end'}>
+                                <Text color={'#666'} textAlign={"right"} fontSize="sm" textDecorationLine={"underLine"} >Forgot password?</Text>
+                            </Stack>
+                            <Button
+                                onClick={(e) => onSubmit(e)}
+                                type={"submit"}
+                                bg={'#dc4c3e'}
+                                color={'white'}
+                                // isLoading={loading}
+                                _hover={{
+                                    bg: '#b22c1f',
+                                }}>
+                                Sign in
+                            </Button>
+                        </Stack>
+                        <Text color={"#666"} textAlign={"center"} fontSize="sm" textDecorationLine={"underLine"}>Don’t have an account? Sign up</Text>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
+    )
+}
--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,9 +1,11 @@
-import { createBrowserRouter, createRoutesFromElements , Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import SidebarLayout from "../pages/SidebarLayout";
 import Today from "../pages/Today/Today";
 import Inbox from "../pages/Inbox";
 import Filters_labels from "../pages/Filters_labels";
 import Upcoming from "../pages/Upcoming";
+import Login from "../pages/auth/Login";
+import { Flex, Image } from "@chakra-ui/react";
 
 
 
@@ -12,12 +14,30 @@ export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={<SidebarLayout />} >
-            <Route index  element={<Today />}/>
-            <Route path={"inbox"}  element={<Inbox />}/>
-            <Route path={"upcoming"}  element={<Upcoming />}/>
-            <Route path={"filters-labels"}  element={<Filters_labels />}/>
+                <Route index element={<Today />} />
+                <Route path={"inbox"} element={<Inbox />} />
+                <Route path={"upcoming"} element={<Upcoming />} />
+                <Route path={"filters-labels"} element={<Filters_labels />} />
             </Route>
-            
-            </>
+
+            <Route path={"/login"} element={
+                <Flex justifyContent={"center"} alignItems={"center"}>
+
+                    <Login />
+
+                    <Image
+                        display={{base: "none" , lg: "block"}}
+                        
+                        width={"450px"}
+                        h="100%"
+                        maxW={"100%"}
+                        objectFit='cover'
+                        src= "../../public/44245fc51c3e2ab05ee6d92c13e2e08a.png"
+                    />
+                </Flex>
+            }
+
+            />
+        </>
     )
 )
