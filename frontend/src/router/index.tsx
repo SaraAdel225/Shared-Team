12e1ef5e3762b4ca4dd639c43e8716dda3cb62c2
Raheeemsala7 <rahemsalah534@gmail.com>
2024-07-16 21:18:20 +0300

    mmm

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -6,6 +6,7 @@ import Filters_labels from "../pages/Filters_labels";
 import Upcoming from "../pages/Upcoming";
 import Login from "../pages/auth/Login";
 import { Flex, Image } from "@chakra-ui/react";
+import Register from "../pages/auth/Register";
 
 
 
@@ -24,6 +25,24 @@ export const router = createBrowserRouter(
                 <Flex justifyContent={"center"} alignItems={"center"}>
 
                     <Login />
+
+                    <Image
+                        display={{base: "none" , lg: "block"}}
+                        
+                        width={"450px"}
+                        h="100%"
+                        maxW={"100%"}
+                        objectFit='cover'
+                        src= "../../public/44245fc51c3e2ab05ee6d92c13e2e08a.png"
+                    />
+                </Flex>
+            }
+
+            />
+            <Route path={"/register"} element={
+                <Flex justifyContent={"center"} alignItems={"center"}>
+
+                    <Register />
 
                     <Image
                         display={{base: "none" , lg: "block"}}
